5983c1a61351fb30faebac4d9c02020015a62f43
edgm2003 <54037279+edgm2003@users.noreply.github.com>
2026-07-30 02:16:03 +0000

    MODELADO DATOS STARWARS TERMINADO

--- src/models.py
+++ src/models.py
@@ -1,19 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import List, Optional
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(120), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    password: Mapped[str] = mapped_column(String(80), nullable=False)
 
+    favoritos: Mapped[List["Favorito"]] = relationship(back_populates="usuarios")
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Personaje(db.Model):
+    __tablename__ = "personajes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(50))
+    gender: Mapped[str] = mapped_column(String(50))
+    eye_color: Mapped[str] = mapped_column(String(50))
+
+    favoritos: Mapped[List["Favorito"]] = relationship(back_populates="personajes")
+
+class Planeta(db.Model):
+    __tablename__ = "planetas"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(String(50))
+    terrain: Mapped[str] = mapped_column(String(50))
+    population: Mapped[str] = mapped_column(String(50))
+
+    favoritos: Mapped[List["Favorito"]] = relationship(back_populates="planetas")
+
+class Favorito(db.Model):
+    __tablename__ = "favoritos"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("usuarios.id"), nullable=False)
+
+    personaje_id: Mapped[Optional[int]] = mapped_column(ForeignKey("personajes.id"), nullable=True)
+    planeta_id: Mapped[Optional[int]] = mapped_column(ForeignKey("planetas.id"), nullable=True)
+
+    usuario: Mapped["Usuario"] = relationship(back_populates="favoritos")
+    personaje: Mapped[Optional["Personaje"]] = relationship(back_populates="favoritos")
+    planeta: Mapped[Optional["Planeta"]] = relationship(back_populates="favoritos")
